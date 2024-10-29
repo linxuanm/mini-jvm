e8d2ceb496ba3d5ac2021acc878df5295b3606b8
davidmaamoaix <davidmaamoaix@gmail.com>
2024-10-29 01:06:56 -0400

    Sample java file

---
+++ sample_java/Hello.java
@@ -0,0 +1,12 @@
+class Hello {
+
+    private static int BONK = 12;
+    public String FOO = "Yay";
+
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        float b = 12;
+        b += 42.5;
+    }
+}
